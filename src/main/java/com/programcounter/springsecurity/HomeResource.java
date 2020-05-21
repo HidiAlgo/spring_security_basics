97588fcf2090c688e0aab7bdd5ef13faadcec6c4
Hashan <hashan.mahesh01@gmail.com>
2020-05-21 18:28:00 +0530

    Files for the spring security basics

---
+++ src/main/java/com/programcounter/springsecurity/HomeResource.java
@@ -0,0 +1,27 @@
+package com.programcounter.springsecurity;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeResource {
+
+    @GetMapping("/")
+    public String getHome(){
+        return "<h1>Welcome</h1>";
+    }
+
+    @GetMapping("/user")
+    public String getUser(){
+        return "<h1>Welcome User</h1>";
+    }
+
+    @GetMapping("/admin")
+    public String getAdmin(){
+        return "<h1>Welcome Admin</h1>";
+    }
+
+}
